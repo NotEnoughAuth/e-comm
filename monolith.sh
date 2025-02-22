08febb257cb16ec7aa54c4c47040d9ba2d601c4d
Owen-Pryga <58037675+NotEnoughAuth@users.noreply.github.com>
2025-02-22 17:52:49 -0600

    Update MySQL root password command in prescripts function to use SET PASSWORD syntax and flush privileges

--- monolith.sh
+++ monolith.sh
@@ -157,7 +157,8 @@ prescripts(){
                         read -p "Confirm the new password: " -s CONFIRM_MYSQL_ROOT_PASSWORD
                         echo
                         if [ "$NEW_MYSQL_ROOT_PASSWORD" == "$CONFIRM_MYSQL_ROOT_PASSWORD" ]; then
-                            mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$NEW_MYSQL_ROOT_PASSWORD';"
+                            mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$NEW_MYSQL_ROOT_PASSWORD');"
+                            mysql -u root -e "FLUSH PRIVILEGES;"
                             MYSQL_ROOT_PASSWORD=$NEW_MYSQL_ROOT_PASSWORD
                             # Change the user in presta config
                             if [ -f /var/www/html/prestashop/config/settings.inc.php ]; then
